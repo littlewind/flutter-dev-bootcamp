2a325bafd881549d3c587a009dca7d85ff2b37f9
littlewind <tranphung1412@gmail.com>
2020-08-18 10:07:34 +0700

    Expanded Widget

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -18,6 +18,20 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
