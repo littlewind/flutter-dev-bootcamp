ce2add59b308b10f34a7f215a47cffe8878b8d2d
littlewind <tranphung1412@gmail.com>
2020-09-17 01:04:03 +0700

    Call API and Pass data to other screen, pass data to State Object

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
+import 'package:clima_flutter/screens/location_screen.dart';
+import 'package:clima_flutter/services/location.dart';
+import 'package:clima_flutter/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '5d4c92faa1c110ab3d996d50264f0a9b';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,15 +14,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    location.latitude = location.latitude;
+    location.longitude = location.longitude;
+
+    NetWorkHelper netWorkHelper = NetWorkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await netWorkHelper.getData();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LocationScreen(
+            locationWeather: weatherData,
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
